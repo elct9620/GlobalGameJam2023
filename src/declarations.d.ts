88dd812aec6184d9a80bdf2d79c6dd5efd73144c
PastLeo <chgu82837@gmail.com>
2023-02-04 03:23:20 +0800

    install & setup Pixi.js & InversifyJS

---
+++ src/declarations.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+declare module "midi-parser-js" {
+  const value: any;
+  export default value;
+}
+
+declare module "*.mid" {
+  const value: any;
+  export default value;
+}
