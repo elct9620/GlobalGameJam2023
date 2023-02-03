8d5b0d04a51d8be10347f97a3a00899732532fb5
PastLeo <chgu82837@gmail.com>
2023-02-04 03:42:26 +0800

    test out inversify

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,6 +1,10 @@
 import { Container } from "inversify";
-// import TYPES from './types';
+import TYPES, { Warrior, Weapon, ThrowableWeapon } from './types';
+import { Ninja, Katana, Shuriken } from "./entities";
 
 var container = new Container();
+container.bind<Warrior>(TYPES.Warrior).to(Ninja);
+container.bind<Weapon>(TYPES.Weapon).to(Katana);
+container.bind<ThrowableWeapon>(TYPES.ThrowableWeapon).to(Shuriken);
 
 export default container;
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,20 @@
+export interface Warrior {
+  fight(): string;
+  sneak(): string;
+}
+
+export interface Weapon {
+  hit(): string;
+}
+
+export interface ThrowableWeapon {
+  throw(): string;
+}
 
 let TYPES = {
+  Warrior: Symbol("Warrior"),
+  Weapon: Symbol("Weapon"),
+  ThrowableWeapon: Symbol("ThrowableWeapon")
 };
 
 export default TYPES;
