23163e836f9e49a4bb6da42f2e8b1c47eca9bd55
蒼時弦也 <elct9620@frost.tw>
2023-02-04 16:50:24 +0800

    Call Hit when input is allowed

--- src/subscribers/InputSubscriber.ts
+++ src/subscribers/InputSubscriber.ts
@@ -1,12 +1,26 @@
-import { injectable } from 'inversify'
+import { inject, injectable } from 'inversify'
 import 'reflect-metadata'
 import { Observer } from 'rxjs'
 
 import { KeyboardEvent } from '../events'
+import { GameUseCase } from '../usecase'
 
 @injectable()
 export class InputSubscriber implements Observer<KeyboardEvent> {
-  next(_event: KeyboardEvent) {
+  static ALLOW_KEYS = [' ']
+
+  private readonly game: GameUseCase
+
+  constructor(
+    @inject(GameUseCase) game: GameUseCase
+  ) {
+    this.game = game
+  }
+
+  next(event: KeyboardEvent) {
+    if(InputSubscriber.ALLOW_KEYS.includes(event.key)) {
+      this.game.Hit()
+    }
   }
 
   error = (_err: Error) => {}
