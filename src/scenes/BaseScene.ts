9c1ce630475f85d5fc53c55d498b3e13425ee128
蒼時弦也 <elct9620@frost.tw>
2023-02-04 11:23:30 +0800

    Add scene hook

--- src/scenes/BaseScene.ts
+++ src/scenes/BaseScene.ts
@@ -4,10 +4,22 @@ import 'reflect-metadata';
 
 @injectable()
 export class BaseScene extends PIXI.Container {
+  constructor() {
+    super()
+
+    this.onCreated()
+  }
+
+  onCreated = () => {}
+  onLoaded = () => {}
+  onUnloaded = () => {}
+
   load = () => {
     if(import.meta.env.DEV) {
       console.info('Loaded Scene: ', this.constructor.name)
     }
+
+    this.onLoaded()
   }
-  unload = () => {}
+  unload = () => this.onUnloaded()
 }
